52638efa0836b49b333a3588b7ed9da0ddec90a1
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-07-03 19:11:22 -0400

    initial commit

--- src/main/java/murach/data/UserIO.java
+++ src/main/java/murach/data/UserIO.java
@@ -9,59 +9,68 @@ import java.util.StringTokenizer;
 
 public class UserIO {
 
-    public static boolean add(User user, String filepath) throws IOException {
+    public static boolean add(User user, String filepath) {
         boolean isAdded = false;
-        System.out.println(filepath);
-        try (var out = new PrintWriter(new FileWriter(filepath, true));) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(filepath, true));) {
             out.println(user.getEmail() + "|" + user.getFirstName() + "|" + user.getLastName());
             isAdded = true;
+        } catch (IOException e) {
+            System.out.println(e);
         }
         return isAdded;
     }
 
-    public static User getUser(String email, String filepath) throws IOException {
-/*        try (var fis = new FileOutputStream(filepath);
+    public static User getUser(String email, String filepath) {
+        /*
+        try (var fis = new FileOutputStream(filepath);
              var bis = new BufferedOutputStream(fis);
              var ois = new ObjectOutputStream(bis)) {
             ois.writeObject("Hello");
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
-        User user = null;
+        }
+        */
         try (BufferedReader in = new BufferedReader(new FileReader(filepath));) {
+            User user = null;
             String line = in.readLine();
             while (line != null) {
-                StringTokenizer t = new StringTokenizer(line, "|");
-                if (t.countTokens() < 3) {
+                StringTokenizer tokenizer = new StringTokenizer(line, "|");
+                if (tokenizer.countTokens() < 3) {
                     return new User("", "", "");
                 }
-                String token = t.nextToken();
+                String token = tokenizer.nextToken();
                 if (token.equalsIgnoreCase(email)) {
-                    String firstName = t.nextToken();
-                    String lastName = t.nextToken();
+                    String firstName = tokenizer.nextToken();
+                    String lastName = tokenizer.nextToken();
                     user = new User(firstName, lastName, email);
                 }
                 line = in.readLine();
             }
+            return user;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
-        return user;
     }
 
-    public static List<User> getUsers(String filepath) throws IOException {
-        List<User> users;
+    public static List<User> getUsers(String filepath) {
         try (BufferedReader in = new BufferedReader(new FileReader(filepath));) {
-            users = new ArrayList<>();
+            List<User> users = new ArrayList<>();
+
             String line = in.readLine();
             while (line != null) {
-                StringTokenizer t = new StringTokenizer(line, "|");
-                String email = t.nextToken();
-                String firstName = t.nextToken();
-                String lastName = t.nextToken();
+                StringTokenizer tokenizer = new StringTokenizer(line, "|");
+                String email = tokenizer.nextToken();
+                String firstName = tokenizer.nextToken();
+                String lastName = tokenizer.nextToken();
                 User user = new User(firstName, lastName, email);
                 users.add(user);
                 line = in.readLine();
             }
+            return users;
+        } catch (IOException e) {
+            System.out.println(e);
+            return null;
         }
-        return users;
     }
 }
